09338bbe215394542e72d4de4514e46e0eb1e80d
Jamie Zimmerman <jamiez@uoregon.edu>
2017-01-12 11:05:51 +0000

    fixed path prefixes + changed postgres version

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,12 +1,12 @@
 #clone postgres repo
-git clone https://github.com/postgres/postgres.git
+git clone -b REL9_5_STABLE https://github.com/postgres/postgres.git
 cd postgres
-./configure
+./configure --prefix=$1
 make
 make install
 curl http://www-us.apache.org/dist//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2
 tar -xjf httpd-2.4.25.tar.bz2
 cd httpd-2.4.25
-./configure --prefix=$HOME/installed
+./configure --prefix=$1
 make
 make install
