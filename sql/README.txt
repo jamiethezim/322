598ccc53927feb36ff8ea74c75209712870daa27
Jamie <jamiez@uoregon.edu>
2017-02-02 14:53:09 +0000

    copied solution code for database creation

--- sql/README.txt
+++ sql/README.txt
@@ -1,5 +1,42 @@
-create_tables.sql -> just a long list of CREATE TABLE statements. Followed the lost_req.pdf for information on the data model. Each table has a primary key, not of data type int but of serial primary key.
+This directory holds some tools for configuring the LOST database and migrating legacy OSNAP data. This project is complex enough that no two people are likely to write the same logic with the same structure. Direct use of this code in your own project will result in 0 points awarded for the data migration portion of the final project.
 
-gen_insert2.py -> take 2 on the previous python script to import all the legacy data. Uses psychopg2 to connect to the database and port, which are user specified arguments. has functions for insert into the assets, asset_at, and facilities table. The most important columns were asset tag and fcode, because that's all the next assignment really cares about.
+Files:
+README.txt - This readme file
+create_tables.sql   - A script to generate the base tables
+import_data.sh      - A script to import OSNAP legacy data
+do_transit.py       - Processes the transit data
+facilities_map.sql  - Loads the static data on the facilities
+norm_tags.py        - Rewrites some of the data so that it is easier to process
+prep_inv.py         - Converts *_inventory.csv to sql
+prep_prod.py        - Converts product info to sql
+prep_sec.py         - Converts security info to sql
+prepend_fcode       - Concatenates the inventory data and annotates with source facility
+waypoints.py        - Processes the convoy file
+backfill.sql        - A script to fill in a few inferrences
 
-import_data.sh -> curls the legacy files locally and unzips them. runs the create_tables sql script, and executes gen_insert2 python script to parse and import all the unzipped legacy data. 
+
+
+-------------------------------------
+-- First cut at queries for testing with OSNAP data imported
+-------------------------------------
+To test the LOST database after the OSNAP data has been migrated, the following queries have been suggests. These queries are untested but should have the right basic structure. If you compare the queries to the entity relationship diagram you should be able to trace the path between the assets and facilities.
+
+/* This query successfully returning all assets at least once is enough to get credit */
+SELECT fcode,asset_tag
+FROM assets a
+LEFT JOIN asset_at aa  ON a.asset_pk=aa.asset_fk
+LEFT JOIN facilities f ON aa.facility_fk=f.facility_pk
+
+/* This query is more complex... only the facility an asset is currently at. Assets in transit will be missed */
+SELECT fcode,asset_tag 
+FROM assets a
+JOIN asset_at aa  ON a.asset_pk=aa.asset_fk
+JOIN facilities f ON aa.facility_fk=f.facility_pk
+WHERE aa.arrive_dt < now() and (aa.depart_dt is NULL or aa.depart_dt > now())
+
+/* This query should pick up the assets currently in transit */
+SELECT request_id,asset_tag 
+FROM assets a
+JOIN asset_on aa  ON a.asset_pk=aa.asset_fk
+JOIN convoys c ON aa.convoy_fk=c.convoy_pk
+WHERE aa.load_dt < now() and (aa.unload_dt is NULL or aa.unload_dt > now())
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,95 +1,105 @@
-CREATE TABLE products(
-	product_pk serial primary key,
-	vendor text,
-	description text,
-	alt_description text
-);
-CREATE TABLE assets(
-	asset_pk serial primary key,
-	product_fk int,
-	asset_tag text,
-	description text,
-	alt_description text
+create table products (
+    product_pk      serial primary key,
+    vendor          varchar(128) not null,
+    description     text,
+    alt_description text,
+    product_name    varchar(128),
+    product_model   varchar(128),
+    price           numeric
 );
 
-CREATE TABLE vehicles(
-	vehicle_pk serial primary key,
-	asset_fk int
+create table assets (
+    asset_pk        serial primary key,
+    product_fk      integer, -- Nullable, not purchased asset
+    asset_tag       varchar(32),
+    description     text,
+    alt_description text
 );
 
-CREATE TABLE facilities(
-	facility_pk serial primary key,
-	fcode text,
-	common_name text,
-	location text
+create table vehicles (
+    vehicle_pk      serial primary key,
+    asset_fk        integer REFERENCES assets(asset_pk) not null -- Convoy vehicles must be OSNAP assets
 );
 
-CREATE TABLE asset_at(
-	asset_fk serial primary key,
-	facility_fk int,
-	arrive_dt timestamp,
-	depart_dt timestamp
+create table facilities (
+    facility_pk     serial primary key,
+    fcode           varchar(16),
+    common_name     varchar(128),
+    location        varchar(128) -- May make this a reference later
 );
 
-CREATE TABLE convoys(
-	convoy_pk serial primary key,
-	request text,
-	source_fk int,
-	dest_fk int,
-	depart_dt timestamp,
-	arrive_dt timestamp
+create table asset_at (
+    asset_fk        integer REFERENCES assets(asset_pk) not null,
+    facility_fk     integer REFERENCES facilities(facility_pk) not null,
+    arrive_dt       timestamp, -- UTC
+    depart_dt       timestamp -- UTC
 );
 
-CREATE TABLE used_by(
-	vehicle_fk serial primary key,
-	convoy_fk int
+create table convoys (
+    convoy_pk       serial primary key,
+    request_id      varchar(16),
+    src_fk          integer REFERENCES facilities(facility_pk) not null,
+    dst_fk          integer REFERENCES facilities(facility_pk) not null,
+    depart_dt       timestamp, -- UTC
+    arrive_dt       timestamp  -- UTC
 );
 
-CREATE TABLE asset_on(
-	asset_fk serial primary key,
-	convoy_fk int,
-	load_dt timestamp,
-	unload_dt timestamp
+create table waypoints (
+    convoy_fk   integer REFERENCES convoys(convoy_pk) not null,
+    point_dt    timestamp --UTC
 );
 
-CREATE TABLE users(
-	user_pk serial primary key,
-	username text,
-	active boolean
+create table used_by (
+    vehicle_fk  integer REFERENCES vehicles(vehicle_pk) not null,
+    convoy_fk   integer REFERENCES convoys(convoy_pk) not null
 );
 
-CREATE TABLE roles(
-	role_pk serial primary key,
-	title text
+create table asset_on (
+    asset_fk    integer REFERENCES assets(asset_pk) not null,
+    convoy_fk   integer REFERENCES convoys(convoy_pk) not null,
+    load_dt     timestamp, -- UTC
+    unload_dt   timestamp  -- UTC
 );
 
-CREATE TABLE user_is(
-	user_fk serial primary key,
-	role_fk int
+create table users (
+    user_pk     serial primary key,
+    username    varchar(64) not null,
+    active      boolean default FALSE
 );
 
-CREATE TABLE user_supports(
-	user_fk serial primary key,
-	facility_fk int
+create table roles (
+    role_pk     serial primary key,
+    title       varchar(32)
 );
 
-CREATE TABLE levels(
-	level_pk serial primary key,
-	abbrv text,
-	comment_ text
+create table user_is (
+    user_fk     integer REFERENCES users(user_pk) not null,
+    role_fk     integer REFERENCES roles(role_pk) not null
 );
 
-CREATE TABLE compartments(
-	compartment_pk serial primary key,
-	abbrv text,
-	comment_ text
+create table user_supports (
+    user_fk     integer REFERENCES users(user_pk) not null,
+    facility_fk integer REFERENCES facilities(facility_pk) not null
 );
 
-CREATE TABLE security_tags(
-	tag_pk serial primary key,
-	level_fk int,
-	compartment_fk int,
-	user_fk int,
-	product_fk int,
-	asset_fk int
+create table sec_levels (
+    level_pk    integer primary key, -- levels have order
+    abbrv       varchar(3),
+    comment     varchar(128)
+);
+
+create table sec_compartments (
+    compartment_pk  serial primary key,
+    abbrv           varchar(8),
+    comment         varchar(128)
+);
+
+create table security_tags (
+    tag_pk          serial primary key,
+    level_fk        integer REFERENCES sec_levels(level_pk) not null,
+    compartment_fk  integer REFERENCES sec_compartments(compartment_pk) not null,
+    -- Need to add a constraint so that exactly one of the following is set
+    user_fk         integer REFERENCES users(user_pk),
+    asset_fk        integer REFERENCES assets(asset_pk),
+    product_fk      integer REFERENCES products(product_pk)
 );
