8bb5b805df4f8777ee6ec42e19c70f1988473090
Jamie <jamiez@uoregon.edu>
2017-02-04 11:35:05 +0000

    wrote method for login functionality

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ cur = conn.cursor()
 def create_user():
 	if request.method == 'GET':
 		#loops back to itself
-		return render_template('login.html')
+		return render_template('create_user.html')
 	elif request.method == 'POST':
 		#get login credentials locally
 		usn = request.form['username']
@@ -27,7 +27,7 @@ def create_user():
 		res = cur.fetchall()
 
 		#if user not in db, add credentials, render html saying user added
-		if res == []: #no match
+		if not res: #no match
 			print('no user')
 			SQL = "INSERT INTO logins (username, password) VALUES (%s, %s)"
 			data = (usn, pwd)
@@ -43,6 +43,38 @@ def create_user():
 		#otherwise user in db and render html saying user exists
 		elif usn == res[0][0] and pwd == res[0][1]:
 			return render_template('yay.html')
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
+def login():
+	if request.method == 'GET':
+		return render_template('login.html')
+	elif request.method == 'POST':
+		usn = request.form['username']		
+		pwd = request.form['password']
+		
+		# I have to do some silly stuff in getting user_pk... this is to make sure
+		# that the input password is associated with their username, and that user
+		# didn't get lucky typing in some random password that exists in the db
+		SQL = "SELECT user_pk, username FROM logins where username = '{}'".format(usn)
+		cur.execute(SQL)
+		res1 = cur.fetchall()
+				
+		SQL = "SELECT user_pk, password FROM logins where password = '{}'".format(pwd)
+		cur.execute(SQL)
+		res2 = cur.fetchall()
+		if not res1 or not res2: #if the query didn't find anything
+			return render_template('fail.html')
+		
+		#parse the data from the query result	
+		found_usn = res1[0][1]
+		found_pwd = res2[0][1]
+		pk1 = res1[0][0]
+		pk2 = res2[0][0]
+		if usn == found_usn and pwd == found_pwd and pk1 == pk2:
+			print("found match: {} {} {}".format(found_usn, found_pwd, pk1))
+			return redirect((url_for('dashboard')))
+		
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
