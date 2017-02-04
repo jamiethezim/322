b62bf8e6e9329d4735362356710ef76c475b8f59
Jamie <jamiez@uoregon.edu>
2017-02-04 04:46:05 +0000

    only runs create_tables script

--- preflight.sh
+++ preflight.sh
@@ -1,10 +1,8 @@
 #! /usr/bin/bash
 
-# This script handles the setup that must occur prior to running LOST
+# This script handles the setup that must occur prior to running web app
 # Specifically this script:
-#    1. creates the database
-#    2. imports the legacy data
-#    3. copies the required source to $HOME/wsgi
+#    1. creates the database tables
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: ./preflight.sh <dbname>"
@@ -12,15 +10,9 @@ if [ "$#" -ne 1 ]; then
 fi
 
 # Database prep
+# All that this preflight script needs to do is set up the db...
+# Andy's script already drops and creates db
 cd sql
 psql $1 -f create_tables.sql
-curl -O https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
-tar -xzf osnap_legacy.tar.gz
-bash ./import_data.sh $1 5432
-rm -rf osnap_legacy osnap_legacy.tar.gz
-cd ..
-
-# Install the wsgi files
-cp -R src/* $HOME/wsgi
 
 
