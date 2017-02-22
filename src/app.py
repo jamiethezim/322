6d04db476227d71634fb44db70f1be4dec12d0a3
Jamie <jamiez@uoregon.edu>
2017-02-22 22:18:05 +0000

    wrote transfer request functionality

--- src/app.py
+++ src/app.py
@@ -86,7 +86,17 @@ def login():
 		if usn == found_usn and pwd == found_pwd and pk1 == pk2:
 			print("found match: {} {} {}".format(found_usn, found_pwd, pk1))
 			session['username'] = usn
+			session['position'] = user_is(pk1)
 			return redirect((url_for('dashboard')))
+
+#helper function -- returns a user's title from their primary key identifier
+def user_is(user_pk):
+	SQL = "SELECT role FROM logins JOIN roles ON role_fk = role_pk WHERE user_pk = %s"
+	data = (user_pk,)
+	cur.execute(SQL, data)
+	return cur.fetchone()[0]
+
+
 
 @app.route('/add_facility', methods=['GET', 'POST'])
 def add_facility():
@@ -219,6 +229,59 @@ def asset_report():
 		return redirect(url_for('asset_report'))
 
 
+@app.route('/transfer_req', methods=['GET', 'POST'])
+def transfer_req():
+	if session['position'] != 'logistics officer':
+		return render_template('not_officer.html')
+	if request.method == 'GET':
+		return render_template('transfer_req.html')
+	elif request.method == 'POST':
+		asset = request.form['asset_tag']
+		src = request.form['src_common_name']
+		dest = request.form['dest_common_name']
+		#flask request object does not have a date time attribute
+		#so I just have to get the clock time when the request comes in
+		time = datetime.datetime.now()
+		
+		#I am aware that I reassign requestor from username to user primary key identifier
+		requestor = session['username']
+		SQL = "SELECT user_pk from logins WHERE username = '{}'".format(requestor)
+		cur.execute(SQL)
+		requestor = cur.fetchone()[0]
+
+		#check if the asset tag, src fac, and dest fac are all in DB
+		SQL = "SELECT asset_pk from assets WHERE asset_tag = '{}'".format(asset)
+		cur.execute(SQL)
+		asset_res = cur.fetchone()[0]
+		
+		SQL = "SELECT facility_pk from facilities WHERE common_name = '{}'".format(src)
+		cur.execute(SQL)
+		src_res = cur.fetchone()[0]
+		
+		SQL = "SELECT facility_pk from facilities WHERE common_name = '{}'".format(dest)
+		cur.execute(SQL)
+		dest_res = cur.fetchone()[0]
+	
+		#validity checks
+		if not asset_res:
+			return '<!DOCTYPE HTML> asset does not exist'
+		if not src_res:
+			return '<!DOCTYPE HTML> source facility does not exist'
+		if not dest_res:
+			return '<!DOCTYPE HTML> destination facility does not exist'
+		
+		#process and insert the request
+		SQL = "INSERT INTO requests (requestor, request_dt, src_fac, dest_fac, asset) VALUES (%s, %s, %s, %s, %s)"
+		data = (requestor, time, src_res, dest_res, asset_res)
+		cur.execute(SQL, data)
+		conn.commit()
+		return '<!DOCTYPE HTML> transfer request successfully submitted'
+
+
+#Extra credit - skeleton implemented only to avoid bugs
+@app.route('/transfer_report', methods=['GET', 'POST'])
+def transfer_report():
+	return render_template('transfer_report.html')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
