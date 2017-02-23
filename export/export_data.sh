59a56e3679d76165aaf2475ce98f9123fe3af380
Jamie <jamiez@uoregon.edu>
2017-02-23 13:08:10 +0000

    removed if statement checking outputdir

--- export/export_data.sh
+++ export/export_data.sh
@@ -2,13 +2,17 @@
 
 # This script exports data currently in the database to csv files
 # will create, users.csv, facilities.csv, assets.csv, and transfers.csv
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Usage: ./export_data.sh <dbname> <output dir>"
     exit;
 fi
-#$1 is dbname
-#$2 is the path for the directory where the files will be read from
 
+#regardless of the outputdir existing, it needs to be an empty directory before we populate it
+rm -rf $2
+mkdir $2
 
 #call python export script
-python3 outward.py
+python3 outward.py $1
+
+#move all the csv files to the location
+mv *.csv $2
