7b67ec6be300c171efdc7e35b210c9705b4bf411
Jamie <jamiez@uoregon.edu>
2017-02-24 16:39:12 +0000

    skeleton for revoking client

---
+++ clients/revoke_user.py
@@ -0,0 +1,7 @@
+import sys
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+#logic for revoking user and interaction with server
+if __name__ == '__main__':
+	main()
