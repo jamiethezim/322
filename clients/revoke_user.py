f629b70a0ed29d99a5963104cd6281694ea7955d
Jamie <jamiez@uoregon.edu>
2017-02-24 18:18:06 +0000

    copied logic from activate client

--- clients/revoke_user.py
+++ clients/revoke_user.py
@@ -2,6 +2,23 @@ import sys
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
 
-#logic for revoking user and interaction with server
+#Usage: python3 revoke_user.py <URL> <username>
+def main():
+	#check arguments
+	if len(sys.argv) < 3:
+		print("Usage: python3 %s <URL> <username>"%sys.argv[0])
+		return None
+
+	#prepare the request
+	args = {'username': sys.argv[2]}
+
+	#Package the request
+	data = urlencode(args)
+	#Make the request
+	req = Request(sys.argv[1]+'revoke_user', data.encode('ascii'), method='POST')
+	res = urlopen(req)
+	
+	recieved = res.read().decode('ascii')
+	print(recieved)
 if __name__ == '__main__':
 	main()
